edb133356cce8b0c85e149261a20ec4bd1a2d34c
Aleks <24192100+aleksishere@users.noreply.github.com>
2023-05-11 21:54:33 +0200

    Changed styling

--- script.js
+++ script.js
@@ -1,67 +1,37 @@
-let imgs = document.querySelectorAll('img');
+const buttons = document.querySelectorAll('button.choice');
+const restartButton = document.querySelector('#restartButton');
 let playerScore = 0;
 let computerScore = 0;
-document.getElementById("playerScore").innerHTML = playerScore;
-document.getElementById("computerScore").innerHTML = computerScore;
 
-imgs.forEach((img) => {
-    img.addEventListener("click", clickPicture);
-})
+buttons.forEach(button => {
+    button.addEventListener("click", playRound);
+});
 
-function clickPicture(img) {
-    playRound(img.target.id, getComputerChoice());
-    checkScore(playerScore, computerScore);
-}
+restartButton.addEventListener("click", resetScore);
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
     switch (choice) {
         case 1:
             return 'rock';
-            break;
         case 2:
             return 'paper';
-            break;
         case 3:
             return 'scissors';
-            break;
         default:
             return 'What happened'
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    let playerChoice = playerSelection;
-    if(playerChoice == computerSelection) {
-        document.getElementById('result').innerHTML = 'Draw';
-    } else if(playerChoice == 'rock' && computerSelection == 'scissors') {
-        playerScore = playerScore + 1;
-        document.getElementById('result').innerHTML = 'You win, Rock beats Scissors';
-    } else if(playerChoice == 'rock' && computerSelection == 'paper') {
-        computerScore = computerScore + 1;
-        document.getElementById('result').innerHTML = 'You lose, Paper beats Rock';
-    } else if(playerChoice == 'scissors' && computerSelection == 'rock') {
-        computerScore = computerScore + 1;
-        document.getElementById('result').innerHTML = 'You lose, Rock beats Scissors';
-    } else if(playerChoice == 'scissors' && computerSelection == 'paper') {
-        playerScore = playerScore + 1;
-        document.getElementById('result').innerHTML = 'You win, Scissors beat Paper';
-    }  else if(playerChoice == 'paper' && computerSelection == 'rock') {
-        playerScore = playerScore + 1;
-        document.getElementById('result').innerHTML = 'You win, Paper beats Rock';
-    } else if(playerChoice == 'paper' && computerSelection == 'scissors') {
-        computerScore = computerScore + 1;
-        document.getElementById('result').innerHTML = 'You lose, Scissors beat Paper';
     }
 }
 
 function checkScore(playerScore, computerScore) {
     document.getElementById("playerScore").innerHTML = playerScore;
     document.getElementById("computerScore").innerHTML = computerScore;
-
     if(playerScore == 5 || computerScore == 5) {
-        imgs.forEach((img) => {
-            img.removeEventListener("click",clickPicture);
+        restartButton.style.display = "inline";
+        document.getElementById("result").style.display = "inline";
+        document.getElementById("winner").style.display = "block";
+        buttons.forEach(button => {
+            button.disabled = true;
         })
     }
     if(playerScore == 5) {
@@ -72,15 +42,43 @@ function checkScore(playerScore, computerScore) {
         document.getElementById("winner").style.color = "red";
     } else {
         document.getElementById("winner").innerHTML = "";
+        restartButton.style.display = "none"
     }
 }
-document.querySelector("button").addEventListener("click", () => {
+
+function playRound() {
+    let computerSelection = getComputerChoice();
+    if(this.id == computerSelection) {
+        document.getElementById('result').innerHTML = 'Draw';
+    } else if(this.id == 'rock' && computerSelection == 'scissors') {
+        playerScore = playerScore + 1;
+        document.getElementById('result').innerHTML = 'You win, Rock beats Scissors';
+    } else if(this.id == 'rock' && computerSelection == 'paper') {
+        computerScore = computerScore + 1;
+        document.getElementById('result').innerHTML = 'You lose, Paper beats Rock';
+    } else if(this.id == 'scissors' && computerSelection == 'rock') {
+        computerScore = computerScore + 1;
+        document.getElementById('result').innerHTML = 'You lose, Rock beats Scissors';
+    } else if(this.id == 'scissors' && computerSelection == 'paper') {
+        playerScore = playerScore + 1;
+        document.getElementById('result').innerHTML = 'You win, Scissors beat Paper';
+    }  else if(this.id == 'paper' && computerSelection == 'rock') {
+        playerScore = playerScore + 1;
+        document.getElementById('result').innerHTML = 'You win, Paper beats Rock';
+    } else if(this.id == 'paper' && computerSelection == 'scissors') {
+        computerScore = computerScore + 1;
+        document.getElementById('result').innerHTML = 'You lose, Scissors beat Paper';
+    }
+    checkScore(playerScore, computerScore);
+}
+
+function resetScore() {
     playerScore = 0;
     computerScore = 0;
-    document.getElementById("winner").innerHTML = ""
-    document.getElementById("result").innerHTML = ""
-    imgs.forEach((img) => {
-        img.addEventListener("click", clickPicture);
+    
+    buttons.forEach(button => {
+        button.disabled = false;
     })
-    checkScore(playerScore,computerScore)
-})
+    document.getElementById("result").innerHTML = "";
+    checkScore(playerScore, computerScore);
+}
